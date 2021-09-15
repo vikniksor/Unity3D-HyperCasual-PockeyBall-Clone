53cec9d783f1f57996315bf7dba0e9024c2a57d7
SuperNewPC\SuperPC <khaustov.maksym@gmail.com>
2021-09-15 17:41:49 +0300

    add ball now if touch or mouse button down - ball jumps and if mouse button up - ball using raycast and if ball straight ahead of block - stops

---
+++ Assets/_Scripts/Ball.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Ball : MonoBehaviour
+{
+    [SerializeField] private float _jumpForce;
+
+    private Rigidbody _rigidbody;
+
+
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            _rigidbody.isKinematic = false;
+            _rigidbody.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            Ray ray = new Ray(transform.position, Vector3.forward);
+            if (Physics.Raycast(ray, out RaycastHit hitInfo))
+            {
+                if (hitInfo.collider.TryGetComponent(out Block block))
+                {
+                    _rigidbody.isKinematic = true;
+                    _rigidbody.velocity = Vector3.zero;
+                }
+            }
+        }
+    }
+}
